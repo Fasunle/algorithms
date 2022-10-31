6975156749514b4e05ddfbbecc08a7bcb9972aaa
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-31 17:46:04 +0100

    fix: name spacing issues

--- searching algorithms/binary-search.ts
+++ searching algorithms/binary-search.ts
@@ -1,31 +1,32 @@
+'strict';
 /**
- * Given an array of integers nums which is sorted in ascending order, and an integer target, 
+ * Given an array of integers nums which is sorted in ascending order, and an integer target,
  * write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
- * 
- * 
+ *
+ *
  * For a linear search, the time complexity is O(log n)
  */
 
-const findTarget = ( arr: number[], target: number ): number => {
-    const searchKeyIndex = Math.floor( arr.length / 2 );    // middle of the array
-    let start = 0;
-    let end = arr.length;
+let findTarget2 = (arr: number[], target: number): number => {
+  const searchKeyIndex = Math.floor(arr.length / 2); // middle of the array
+  let start = 0;
+  let end = arr.length;
 
-    while ( start != end ) {
-        const searchKey = arr[ searchKeyIndex ];
-        if ( target === searchKey ) return searchKeyIndex;
-        
-        if ( target < searchKey ) {
-            end = searchKeyIndex - 1;
-        } else {
-            start = searchKeyIndex + 1;
-        }
+  while (start != end) {
+    const searchKey = arr[searchKeyIndex];
+    if (target === searchKey) return searchKeyIndex;
+
+    if (target < searchKey) {
+      end = searchKeyIndex - 1;
+    } else {
+      start = searchKeyIndex + 1;
     }
+  }
 
-    return -1;
-}
+  return -1;
+};
 
-const arr = [ 1, 2, 3, 4, 7, 12, 67, 34 ];
-const target = 12;
+let arr2 = [1, 2, 3, 4, 7, 12, 67, 34];
+let target2 = 12;
 
-console.log( findTarget( arr, target ) );
+console.log(findTarget2(arr2, target2));
--- searching algorithms/linear-search.ts
+++ searching algorithms/linear-search.ts
@@ -1,20 +1,20 @@
 /**
- * Given an array of integers nums which is sorted in ascending order, and an integer target, 
+ * Given an array of integers nums which is sorted in ascending order, and an integer target,
  * write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
- * 
- * 
+ *
+ *
  * For a linear search, the time complexity is O(n)
  */
 
-const findTarget = ( arr: number[], target: number ): number => {
-    let foundTargetIndex = -1;
-    for ( let i = 0; i < arr.length; i++ ){
-        if ( target === arr[ i ] ) return i;
-    }
-    return foundTargetIndex;
-}
+const findTarget1 = (arr: number[], target: number): number => {
+  let foundTargetIndex = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (target === arr[i]) return i;
+  }
+  return foundTargetIndex;
+};
 
-const arr = [ 1, 2, 3, 4, 7, 12, 67, 34 ];
-const target = 12;
+const arr1 = [1, 2, 3, 4, 7, 12, 67, 34];
+const target1 = 12;
 
-console.log( findTarget( arr, target ) );
+console.log(findTarget1(arr1, target1));
